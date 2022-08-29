bd83eb1f6b4ed57cd8d7b91fbb47948ee8dde988
fouz0062 <fouzifathii@gmail.com>
2022-08-29 11:32:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Destination } from "./Pages/Destination";
+import { PrivateTour } from "./Pages/PrivateTour";
+import { SingleTour } from "./Pages/SingleTour";
+import { TourList } from "./Pages/TourList";
+import { Template } from "./Pages/Template";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Template />}>
+            <Route index element={<Home />}></Route>
+            <Route path="Destination" element={<Destination />}></Route>
+            <Route path="PrivateTour" element={<PrivateTour />}></Route>
+            <Route path="SingleTour" element={<SingleTour />}></Route>
+            <Route path="TourList" element={<TourList />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
